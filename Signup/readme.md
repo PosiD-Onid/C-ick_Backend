cc8eb713d79fe4d38bc9bf1aba502fde7dbb3284
caffeine <139735428+Ceffeina@users.noreply.github.com>
2024-06-27 20:13:01 +0900

    Delete Signup directory

--- Signup/readme.md
+++
@@ -1,2 +0,0 @@
-# Click_Backend
-# Click_Backend
--- Signup/router/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const teacherRouter = require('./teacher')
-const router = express.Router();
-
-router.use('/teacher', teacherRouter);
-
-
-
-module.exports= router;
